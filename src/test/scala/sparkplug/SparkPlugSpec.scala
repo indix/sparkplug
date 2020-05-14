a36e3358c466601f5b4fea1bc77056aab3c04767
Ashwanth Kumar <ashwanth.kumar@avalara.com>
2020-05-14 17:30:09 +0530

    Using 4 tabs instead of 2

--- src/test/scala/sparkplug/SparkPlugSpec.scala
+++ src/test/scala/sparkplug/SparkPlugSpec.scala
@@ -98,9 +98,9 @@ trait SpecAccumulatorsSparkListener extends ScalaFutures {
 }
 
 class SparkPlugSpec
-  extends FlatSpec
-  with Matchers
-  with SpecAccumulatorsSparkListener {
+    extends FlatSpec
+    with Matchers
+    with SpecAccumulatorsSparkListener {
 
   // Ref - https://issues.apache.org/jira/browse/SPARK-22918
   System.setSecurityManager(null)
